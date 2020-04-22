e5999c91ebcd0df5d7dbb3788a838f24a9a7ce06
Abeautifulsnow <1491444340@qq.com>
2020-04-23 00:16:12 +0800

    add file

---
+++ 日常练习脚本/mro.py
@@ -0,0 +1,14 @@
+class A():
+    def foo1(self):
+        print("A")
+class B(A):
+    def foo2(self):
+        pass
+class C(A):
+    def foo1(self):
+        print("C")
+class D(B, C):
+    pass
+
+d = D()
+d.foo1()
