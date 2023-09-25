cc52e776ef1fc58c3596b0752c3fe240016cce20
Abeautifulsnow <1491444340@qq.com>
2023-09-25 14:40:28 +0800

    Update 归并排序.py

--- sort_algorithm/归并排序.py
+++ sort_algorithm/归并排序.py
@@ -54,6 +54,36 @@ def merge1(left: typing.List[int], right: typing.List[int]):
 
     return result
 
+# Space complexity: O(1)
+def merge_sort(xs):
+    """Inplace merge sort of array without recursive. The basic idea
+    is to avoid the recursive call while using iterative solution.
+    The algorithm first merge chunk of length of 2, then merge chunks
+    of length 4, then 8, 16, .... , until 2^k where 2^k is large than
+    the length of the array
+    """
+
+    unit = 1
+    while unit <= len(xs):
+        h = 0
+        for h in range(0, len(xs), unit * 2):
+            l, r = h, min(len(xs), h + 2 * unit)
+            mid = h + unit
+            # merge xs[h:h + 2 * unit]
+            p, q = l, mid
+            while p < mid and q < r:
+                if xs[p] < xs[q]:
+                    p += 1
+                else:
+                    tmp = xs[q]
+                    xs[p + 1 : q + 1] = xs[p:q]
+                    xs[p] = tmp
+                    p, mid, q = p + 1, mid + 1, q + 1
+
+        unit *= 2
+
+    return xs
+
 
 if __name__ == "__main__":
     nums = input("请输入一串数字,  数字之间空一个间隔：")
